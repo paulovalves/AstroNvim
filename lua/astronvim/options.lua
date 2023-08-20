f8824e8ce4685aceaccc49d983dcdb5e9c11395d
paulovalves <paulovalves@outlook.com>
2023-08-20 18:10:54 -0300

    add copilot and my config

--- lua/astronvim/options.lua
+++ lua/astronvim/options.lua
@@ -1,6 +1,9 @@
 vim.opt.viewoptions:remove "curdir" -- disable saving current directory with views
 vim.opt.shortmess:append { s = true, I = true } -- disable startup message
 vim.opt.backspace:append { "nostop" } -- don't stop backspace at insert
+vim.opt.guicursor = "n-i-c:block"
+vim.g.copilot_no_tab_map= true
+vim.api.nvim_set_keymap('i', '<C-l>', 'copilot#Accept("<CR>")', {expr=true, silent=true})
 if vim.fn.has "nvim-0.9" == 1 then
   vim.opt.diffopt:append "linematch:60" -- enable linematch diff algorithm
 end
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -133,6 +133,11 @@ return {
         follow_current_file = { enabled = true },
         hijack_netrw_behavior = "open_current",
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false
+        }
       },
       event_handlers = {
         {
